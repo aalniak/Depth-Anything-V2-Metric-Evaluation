da94d7f5568bb8f4842f639c497f04196a736f85
aalniak <ardaalniak2001@gmail.com>
2025-03-16 15:41:12 +0300

    Add required installations

--- metric_depth/kitti_test.sh
+++ metric_depth/kitti_test.sh
@@ -1,3 +1,4 @@
+pip install kagglehub
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate Depth-Anything-V2
 python kitti_test.py  --encoder vitl   --load-from checkpoints/depth_anything_v2_metric_vkitti_vitl.pth   --max-depth 80  --input-size 518 
--- metric_depth/nyu_test.sh
+++ metric_depth/nyu_test.sh
@@ -1,3 +1,4 @@
+pip install huggingface
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate Depth-Anything-V2
 python nyu_test.py  --encoder vitl   --load-from checkpoints/depth_anything_v2_metric_hypersim_vitl.pth   --max-depth 20  --input-size 518 
